eb0962bf34106fc93c050cc5ea299ef73128c3b7
niksein007 <markpearceniksein@gmail.com>
2020-03-12 14:12:28 +0000

    test

--- src/components/calDisplay.js
+++ src/components/calDisplay.js
@@ -5,27 +5,29 @@ const Cal = (props)=>{
 
     return(
         <div onClick={props.maths} className="cal">
-            <div className=" box" id= "box-0">0</div>
-            <div className="box " id="box-1">1</div>
-            <div className="box " id="box-2">2</div>
-            <div className="box " id="box-3">3</div>
-            <div className="box " id="box-4">4</div>
-            <div className="box " id="box-5">5</div>
-            <div className="box " id="box-6">6</div>
-            <div className="box " id="box-7">7</div>
-            <div className="box " id="box-8">8</div>
-            <div className="box " id="box-9">9</div>
-            <div className="box " id="box-dot">.</div>
-            <div className="box " id="box-add">+</div>
-            <div className="box " id="box-minus">-</div>
-            <div className="box " id="box-multiply">*</div>
-            <div className="box " id="box-division">/</div>
-            <div className="box " id="box-clear">clear</div>
-            <div className="box " id="box-display">
+            <div className=" box box-0" value="0">0</div>
+            <div className="box box-1" value="1">1</div>
+            <div className="box box-2" value="2">2</div>
+            <div className="box box-3" value="3">3</div>
+            <div className="box box-4" value="4">4</div>
+            <div className="box box-5" value="5">5</div>
+            <div className="box box-6" value="6">6</div>
+            <div className="box box-7" value="7">7</div>
+            <div className="box box-8" value="8">8</div>
+            <div className="box box-9" value="9">9</div>
+            <div className="box box-dot" value=".">.</div>
+            <div className="box box-add" value="+">+</div>
+            <div className="box box-minus" value="-">-</div>
+            <div className="box box-multiply" value="*">*</div>
+            <div className="box box-division" value="/">/</div>
+            <div className="box box-clear" value="clear">clear</div>
+                        {/* this is the display section of the browser */}
+            <div className="box box-display" value="display">
             <span id="second">{props.secondDisplay}</span>
             <span id="first">{props.display}</span>
             </div>
-            <div className="box " id="box-equals">=</div>
+
+            <div className="box box-equals" value="equals">=</div>
         </div>
     )
 }
