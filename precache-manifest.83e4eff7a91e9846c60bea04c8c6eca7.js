4d9bba7ef9bb3169dac434e981b57ae6fb57e05b
niksein007 <markpearceniksein@gmail.com>
2020-03-12 17:34:42 +0000

    Updates

---
+++ precache-manifest.83e4eff7a91e9846c60bea04c8c6eca7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "143bc003221986010bcd182ac059c8bf",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "845b3d5cf48dc34c0a03",
+    "url": "/calculator/static/css/main.2df54766.chunk.css"
+  },
+  {
+    "revision": "0e75804ff0b386212c36",
+    "url": "/calculator/static/js/2.576629cc.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/calculator/static/js/2.576629cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "845b3d5cf48dc34c0a03",
+    "url": "/calculator/static/js/main.0075dd6f.chunk.js"
+  },
+  {
+    "revision": "ccfc56ce1b0c91cb76b8",
+    "url": "/calculator/static/js/runtime-main.627f4c10.js"
+  }
+]);
