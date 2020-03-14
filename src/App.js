cfa7fa600e10de042b655d8a3cb019f455bd19bb
niksein007 <markpearceniksein@gmail.com>
2020-03-14 20:30:27 +0000

    more css updates

--- src/App.js
+++ src/App.js
@@ -1,92 +1,85 @@
-import React from 'react';
-import './App.css';
-import Cal from './components/calDisplay'
-
+import React from "react";
+import "./App.css";
+import Cal from "./components/calDisplay";
 
 class App extends React.Component {
- state ={
- display:"",
- secondDisplay:"",
- sign:""
- }
+  state = {
+    display: "",
+    secondDisplay: "",
+    sign: ""
+  };
 
+  maths = event => {
+    //used getAttribue because value is not a predefined attribute of a div tag
+    let value = event.target.getAttribute("value");
 
-  
- 
-
-   maths=(event)=>{
-     
-     //used getAttribue because value is not a predefined attribute of a div tag
-     let value = event.target.getAttribute("value")
-   
     //  console.log(value)
 
-     switch (value) {
-       case "+":
-        case "-":
-          case "*":
-            case "/":
-        this.setState((prevstate)=>{
-          return{
-            sign:value,
-            secondDisplay:prevstate.display + value,
-            display:""
-          
-          }
-        })
-         break;
-         case "equals":
-           let firstInput = parseFloat(this.state.secondDisplay)
-           firstInput =isNaN(firstInput)
-          firstInput ? this.setState({
-            display:"",
-                secondDisplay:"",
-                sign:""
-          }):
-           
-          this.setState((prevstate)=>{
-            return{
-              secondDisplay:prevstate.secondDisplay + prevstate.display,
-              display:parseFloat(prevstate.secondDisplay) + parseFloat(prevstate.display)
-          }
-        }
-          )
-         break;
-
-         case "clear":
-           this.setState({
-            display:"",
-            secondDisplay:"",
-            sign:""
-           })
-         break;
-
-         case "display":
-          //do notting when display is selected
-         break;
-     
-       default:
-        this.setState((prevstate)=>{
+    switch (value) {
+      case "+":
+      case "-":
+      case "*":
+      case "/":
+        this.setState(prevstate => {
           return {
-            display:prevstate.display + value
-          }
-        })
-         break;
-     }
+            sign: value,
+            secondDisplay: prevstate.display + value,
+            display: ""
+          };
+        });
+        break;
+      case "equals":
+        let firstInput = parseFloat(this.state.secondDisplay);
+        firstInput = isNaN(firstInput);
+        firstInput
+          ? this.setState({
+              display: "",
+              secondDisplay: "",
+              sign: ""
+            })
+          : this.setState(prevstate => {
+              return {
+                secondDisplay: prevstate.secondDisplay + prevstate.display,
+                display:
+                  parseFloat(prevstate.secondDisplay) +
+                  parseFloat(prevstate.display)
+              };
+            });
+        break;
 
-   }
-    
+      case "clear":
+        this.setState({
+          display: "",
+          secondDisplay: "",
+          sign: ""
+        });
+        break;
 
-  render(){
-  return (
-    <div className="App">
-     <Cal maths={this.maths}
-      display ={this.state.display}
-        secondDisplay = {this.state.secondDisplay}
-      />
-    </div>
-  );
-}
+      case "display":
+        //do notting when display is selected
+        break;
+
+      default:
+        this.setState(prevstate => {
+          return {
+            display: prevstate.display + value
+          };
+        });
+        break;
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Cal
+          maths={this.maths}
+          display={this.state.display}
+          secondDisplay={this.state.secondDisplay}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
